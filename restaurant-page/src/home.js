f28878c83dd06cabbc058d00a4b60a53aa1f5a95
Enoch Hsu <1529929+ewoknock@users.noreply.github.com>
2024-03-23 20:11:55 -0700

    Adds blurb to home module

--- restaurant-page/src/home.js
+++ restaurant-page/src/home.js
@@ -2,7 +2,7 @@ import nav from './nav';
 import footer from './footer';
 
 let home = () => {
-    let content = document.getElementById('content');
+    let content = document.querySelector('body');
     
     if(!document.querySelector('nav')){
         let navbar = nav();
@@ -11,6 +11,18 @@ let home = () => {
     let home = document.getElementById('home');
     home.classList.add('active');
 
+    let container = document.createElement('main');
+    container.classList.add('container');
+
+    let header = document.createElement('h1');
+    header.innerText = '10 KBBQ';
+    container.appendChild(header);
+
+    let tagline = document.createElement('p');
+    tagline.innerText = `What is 10 KBBQ? This is a Korean restaurant where you can enjoy 10 different types of high quality wagyu beef at a BBQ table`;
+    container.appendChild(tagline);
+
+    content.appendChild(container);
 
     if(!document.querySelector('footer')){
         footer();
