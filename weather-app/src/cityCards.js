2ef05054b4e14065c129dd34546ca2fb0c59cf7e
Enoch Hsu <1529929+ewoknock@users.noreply.github.com>
2024-04-02 16:46:27 -0700

    Adds weather data to city cards after search

--- weather-app/src/cityCards.js
+++ weather-app/src/cityCards.js
@@ -1,8 +1,9 @@
 import { clearContent } from "./display";
 import makeDetailsPage from "./cityDetailsPage";
 import { getCityWeather, getCityForecast } from "./weatherAPI";
+import { kelvinToFahrenheit } from "./DataConvertor";
 
-const makeCard = (cardContainer, city) => {
+const makeCard = async(cardContainer, city) => {
     const card = document.createElement('div')
     card.classList.add('card')
     card.setAttribute('data-latitude', city.lat)
@@ -11,16 +12,30 @@ const makeCard = (cardContainer, city) => {
     const cardHeader = document.createElement('h1')
     cardHeader.classList.add('card-header')
     cardHeader.append(`${city.name}${city.state ? `, ${  city.state}` : ''}${city.country ? `, ${  city.country}` : ''}`)
+    
+    card.append(cardHeader)
+    cardContainer.append(card)
+
+    const {latitude, longitude} = card.dataset
+    const cityWeather = await getCityWeather(latitude, longitude)
+
+    const cardWeather = document.createElement('div')
+    cardWeather.classList.add('card-weather')
+    cardWeather.append(cityWeather.weather[0].main)
+    card.append(cardWeather)
+
+    const cardTemperature = document.createElement('div')
+    cardTemperature.classList.add('card-temperature')
+    cardTemperature.append(`${kelvinToFahrenheit(cityWeather.main.temp)}°F`)
+    card.append(cardTemperature)
 
     card.addEventListener('click', async() => {
-        const {latitude, longitude} = card.dataset
-        const cityWeather = await getCityWeather(latitude, longitude)
+
         const cityForecast = await getCityForecast(latitude, longitude)
         clearContent()
         makeDetailsPage(cityWeather, cityForecast.list.slice(0,5))
     })
-    card.append(cardHeader)
-    cardContainer.append(card)
+
 
 }
 
@@ -28,7 +43,7 @@ const makeCityCards = (cityList) => {
     const cardContainer = document.createElement('div')
     cardContainer.classList.add('card-container')
 
-    cityList.forEach((city, index) => makeCard(cardContainer, city, index))
+    cityList.forEach((city) => makeCard(cardContainer, city))
 
     return cardContainer
 }
