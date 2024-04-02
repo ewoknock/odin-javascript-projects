427ec7e9f910ea15352bf5d51cc5306182eb4bad
Enoch Hsu <1529929+ewoknock@users.noreply.github.com>
2024-04-01 21:01:06 -0700

    Creates detailed city weather card and stylizes

--- weather-app/src/cityDetailsPage.js
+++ weather-app/src/cityDetailsPage.js
@@ -1,7 +1,31 @@
+import { kelvinToCelsius, kelvinToFahrenheit } from "./temperatureConvertor"
+
+const makeDetailsSummaryCard = (cityWeather) => {
+    const currentTime = new Date(Date.now())
+
+    const detailsSummary = document.querySelector('.details-summary')
+
+    const detailsSummaryHeader = document.querySelector('.details-summary-header')
+    detailsSummaryHeader.innerHTML = `<strong>${cityWeather.name}</strong> <span>as of ${currentTime.toLocaleTimeString()}</span>`
+
+    const detailsTemperature = document.querySelector('.content-temperature')
+    detailsTemperature.innerText = `${kelvinToFahrenheit(cityWeather.main.temp)}°`
+    
+    const detailsWeather = document.querySelector('.content-weather')
+    detailsWeather.innerText = `${cityWeather.weather[0].main}`
+
+    const detailsHighLow = document.querySelector('.content-high-low')
+    detailsHighLow.innerText = `High ${kelvinToFahrenheit(cityWeather.main.temp_max)}° • Low ${kelvinToFahrenheit(cityWeather.main.temp_min)}°`
+}
+
 const makeDetailsPage = (cityWeather) => {
     const container = document.querySelector('main');
+    const template = document.querySelector('#details-template')
+    const detailsTemplate = document.importNode(template.content, true);
+    container.append(detailsTemplate);
+
+    makeDetailsSummaryCard(cityWeather)
     console.log(cityWeather);
-    container.append("Hi");
 }
 
 export default makeDetailsPage
